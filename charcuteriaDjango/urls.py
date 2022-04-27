b569af83420bfd3a6e8a3143ebd23edf271b59c5
ikersnc <ikersanchez51@gmail.com>
2022-04-27 13:34:57 +0200

    migracion a django

--- charcuteriaDjango/urls.py
+++ charcuteriaDjango/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from charcuteria import views
 
 urlpatterns = [
+    path('', views.index),
     path('charcuteria/', include('charcuteria.urls')),
     path('admin/', admin.site.urls),
 ]
